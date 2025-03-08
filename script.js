df5133644ff0cd9d38ee00991e3c329642fbf1cd
avi-codesmith <avi.codesmith@gmail.com>
2025-03-08 19:34:10 +0530

    work in progressing

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+let progress = document.querySelector("#progress");
+console.log(progress);
+
+let song = document.querySelector("audio");
+console.log(song);
+
+let playIcon = document.querySelector(".big-icon");
+const background = document.querySelector(".big");
+console.log(playIcon);
+
+song.onloadedmetadata = () => {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+};
+
+const playPause = () => {
+  if (playIcon.src.includes("play.png")) {
+    playIcon.src = "pause.png";
+    song.play();
+  } else if (playIcon.src.includes("pause.png")) {
+    playIcon.src = "play.png";
+    song.pause();
+  }
+};
+
+background.addEventListener("click", playPause);
+
+if (song.play()) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
+}
